5ee74cda6858f8b071ea1b29d042c9967570e7f5
me-naresh141 <me.naresh141@gmail.com>
2022-06-28 14:54:54 +0530

    Disco-Grid

---
+++ index.js
@@ -0,0 +1,36 @@
+let body = document.querySelector('body')
+let bigDiv = document.createElement('div')
+bigDiv.className = 'bigDiv'
+let heading = document.createElement('h1')
+heading.classList.add('heading')
+heading.innerText = '😰😰Disco Grid🥵🥵'
+body.append(heading, bigDiv)
+
+let allBox = []
+
+for (let i = 1; i <= 500; i++) {
+  allBox.push(document.createElement('div'))
+}
+
+function rendomColor() {
+  let red = Math.floor(Math.random() * 255)
+  let green = Math.floor(Math.random() * 255)
+  let blue = Math.floor(Math.random() * 255)
+  let color = `rgb(${red},${green},${blue})`
+  return color
+}
+function generateBox() {
+  allBox.forEach((elm) => {
+    var smalldiv = elm
+    smalldiv.classList.add('smalldiv')
+    smalldiv.innerText = Math.floor(Math.random() * 500)
+    smalldiv.style.background = rendomColor()
+    bigDiv.append(smalldiv)
+  })
+}
+
+bigDiv.addEventListener('mousemove', (event) => {
+  generateBox()
+})
+
+generateBox()
